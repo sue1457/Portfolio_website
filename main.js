7bc365947d8d0120292ef256d9b34c4a3b73e784
sue1457 <sue1457@gamil.com>
2021-08-06 22:48:32 +0900

    commit5

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ const menu = document.querySelector('.navbar__menu');
 toggleBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
 })
+
+const saTriggerMargin = 50;
+const saElementList = document.querySelectorAll('.sa');
+
+const saFunc = function() {
+    for (const element of saElementList) {
+        if (!element.classList.contains('show')) {
+            if (window.innerHeight > element.getBoundingClientRect().top + saTriggerMargin) {
+                element.classList.add('show');
+            }
+        }
+    }
+}
+
+window.addEventListener('load', saFunc);
+window.addEventListener('scroll', saFunc);
